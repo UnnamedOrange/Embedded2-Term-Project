52ce8fe118aba5426952fbb2e55124d0e871ca3e
UnnamedOrange <lycheng1215@sina.com>
2023-11-24 22:37:30 +0800

    Create Main class

--- embedded/src/main.cpp
+++ embedded/src/main.cpp
@@ -1,11 +1,45 @@
 #include <cstdio>
 #include <iostream>
+#include <memory>
 
 extern "C"
 {
+#include <plic.h>
+#include <sysctl.h>
 }
 
+class Main {
+public:
+    Main() {
+        initialize();
+    }
+
+    // Private extracted methods.
+private:
+    void initialize() {
+        initialize_clock();
+        initialize_power();
+        initialize_io();
+        initialize_irq();
+    }
+    void initialize_power() {
+        sysctl_set_power_mode(SYSCTL_POWER_BANK6, SYSCTL_POWER_V18);
+        sysctl_set_power_mode(SYSCTL_POWER_BANK7, SYSCTL_POWER_V18);
+    }
+    void initialize_io() {
+        // TODO.
+    }
+    void initialize_clock() {
+        sysctl_pll_set_freq(SYSCTL_PLL0, 800000000UL);
+        sysctl_pll_set_freq(SYSCTL_PLL1, 400000000UL);
+        sysctl_clock_enable(SYSCTL_CLOCK_AI);
+    }
+    void initialize_irq() {
+        plic_init();
+        // TODO.
+    }
+};
+
 int main() {
-    while (true)
-        ;
+    [[maybe_unused]] const auto _ = std::make_unique<Main>();
 }
