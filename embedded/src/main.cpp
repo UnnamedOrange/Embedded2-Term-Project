f1c6ddf3f01607d2b7bb9e3b54008141a90a6e21
UnnamedOrange <lycheng1215@sina.com>
2023-11-29 19:24:04 +0800

    Enable LCD SPI
    Update main.cpp

--- embedded/src/main.cpp
+++ embedded/src/main.cpp
@@ -32,6 +32,13 @@ private:
         sysctl_set_power_mode(SYSCTL_POWER_BANK7, SYSCTL_POWER_V18);
     }
     void initialize_io() {
+        // LCD SPI.
+        fpioa_set_function(LCD_DC_PIN, fpioa_function_t(int(FUNC_GPIOHS0) + LCD_DC_IO));
+        fpioa_set_function(LCD_CS_PIN, FUNC_SPI0_SS3);
+        fpioa_set_function(LCD_RW_PIN, FUNC_SPI0_SCLK);
+        fpioa_set_function(LCD_RST_PIN, fpioa_function_t(int(FUNC_GPIOHS0) + LCD_RST_IO));
+        sysctl_set_spi0_dvp_data(1);
+
         // LCD backlight.
         fpioa_set_function(LCD_BLIGHT_PIN, fpioa_function_t(int(FUNC_GPIOHS0) + LCD_BLIGHT_IO));
         gpiohs_set_drive_mode(LCD_BLIGHT_IO, GPIO_DM_OUTPUT);
