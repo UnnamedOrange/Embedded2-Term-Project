64e245f6c1044eef0a8cf77c956dc82a748e0d09
UnnamedOrange <lycheng1215@sina.com>
2023-11-29 20:56:17 +0800

    Initialize DVP

--- embedded/src/main.cpp
+++ embedded/src/main.cpp
@@ -22,12 +22,34 @@ extern "C"
 using namespace std::literals;
 
 class Main {
+    using Self = Main;
+
 public:
     static constexpr auto LCD_WIDTH = 320;
     static constexpr auto LCD_HEIGHT = 240;
 
 private:
+    static int irq_dvp(void* ctx) {
+        auto& self = *reinterpret_cast<Main*>(ctx);
+
+        if (dvp_get_interrupt(DVP_STS_FRAME_FINISH)) {
+            dvp_config_interrupt(DVP_CFG_START_INT_ENABLE | DVP_CFG_FINISH_INT_ENABLE, 0);
+            dvp_clear_interrupt(DVP_STS_FRAME_FINISH);
+            self.has_dvp_finished = true;
+        } else {
+            dvp_start_convert();
+            dvp_clear_interrupt(DVP_STS_FRAME_START);
+        }
+
+        return 0;
+    }
+
+private:
     std::array<uint16_t, LCD_WIDTH * LCD_HEIGHT> g_lcd_gram{};
+    std::array<uint16_t, LCD_WIDTH * LCD_HEIGHT> g_dvp_565{};
+    std::array<std::array<uint8_t, LCD_WIDTH * LCD_HEIGHT>, 3> g_dvp_888_planar{};
+
+    volatile bool has_dvp_finished = true;
 
 public:
     Main() {
@@ -40,6 +62,7 @@ private:
         initialize_clock();
         initialize_power();
         initialize_io();
+        initialize_dvp();
         initialize_irq();
     }
     void initialize_power() {
@@ -79,12 +102,49 @@ private:
         sysctl_pll_set_freq(SYSCTL_PLL1, 400000000UL);
         sysctl_clock_enable(SYSCTL_CLOCK_AI);
     }
+    void initialize_dvp() {
+        dvp_init(8);
+        dvp_set_xclk_rate(24000000);
+        dvp_enable_burst();
+        dvp_set_output_enable(DVP_OUTPUT_AI, 1);
+        dvp_set_output_enable(DVP_OUTPUT_DISPLAY, 1);
+        dvp_set_image_format(DVP_CFG_RGB_FORMAT);
+        dvp_set_image_size(320, 240);
+
+        dvp_set_display_addr(reinterpret_cast<uintptr_t>(g_dvp_565.data()));
+        dvp_set_ai_addr(reinterpret_cast<uintptr_t>(g_dvp_888_planar[0].data()),
+                        reinterpret_cast<uintptr_t>(g_dvp_888_planar[1].data()),
+                        reinterpret_cast<uintptr_t>(g_dvp_888_planar[2].data()));
+
+        gc0328_init();
+    }
     void initialize_irq() {
         plic_init();
-        // TODO.
+
+        // DVP.
+        dvp_config_interrupt(DVP_CFG_START_INT_ENABLE | DVP_CFG_FINISH_INT_ENABLE, 0);
+        dvp_disable_auto();
+        plic_set_priority(IRQN_DVP_INTERRUPT, 1);
+        plic_irq_register(IRQN_DVP_INTERRUPT, &Self::irq_dvp, this);
+        plic_irq_enable(IRQN_DVP_INTERRUPT);
+
+        sysctl_enable_irq();
     }
     void bitblt() {
         lcd_draw_picture(0, 0, LCD_WIDTH, LCD_HEIGHT, reinterpret_cast<uint32_t*>(g_lcd_gram.data()));
+    }
+    void capture_blocking(int camera_id) {
+        if (camera_id == 0) {
+            open_gc0328_0();
+        } else {
+            open_gc0328_1();
+        }
+
+        has_dvp_finished = false;
+        dvp_clear_interrupt(DVP_STS_FRAME_START | DVP_STS_FRAME_FINISH);
+        dvp_config_interrupt(DVP_CFG_START_INT_ENABLE | DVP_CFG_FINISH_INT_ENABLE, 1);
+        while (!has_dvp_finished)
+            ;
     }
 };
 
